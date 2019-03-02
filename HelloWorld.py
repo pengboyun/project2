6561a25774114f68fca04f75f15a8ebd0bd607e8
Administrator <2147217957@qq.com>
2019-03-02 16:52:02 +0800

    创建测试

---
+++ HelloWorld.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+import pygame, sys
+from pygame.locals import *
+
+white = 255,255,255
+black = 0,0,0
+print("Hello 彭博云")
+pygame.init()
+
+screen = pygame.display.set_mode((600, 500))
+myfront = pygame.font.Font(None,60)
+textImage = myfront.render("Hello Pygame ", True, black)
+
+while True:
+    for event in pygame.event.get():
+        if event.type in (QUIT, KEYDOWN):
+            sys.exit()
+    screen.fill(white)
+    screen.blit(textImage, (100, 100))
+    pygame.display.update()
