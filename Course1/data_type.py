521613d374c8b78df476327a52d31038b71ad9cb
Administrator <2147217957@qq.com>
2019-03-03 09:30:43 +0800

    提交测试

---
+++ Course1/data_type.py
@@ -0,0 +1,19 @@
+# 字符串 string
+print("hi "+" 1=1")
+print(234,"dcsgfdgs"+"ffff")
+print(type("string"))
+
+# 整数 integer
+print(1)
+print(132,323,688+695)
+print(type(7))
+
+# 浮点数 float
+print(1.0)
+print(1.2,1.4,1.7)
+print(type(1.11))
+
+# 布尔 bool
+print(True,False)
+print(1==1,1!=1)
+print(type(False))
