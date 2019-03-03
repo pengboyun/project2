a3e7216f31e52ffc736b9ca813fa6a617254b08f
Administrator <2147217957@qq.com>
2019-03-03 15:44:07 +0800

    提交测试

--- Course1/list.py
+++ Course1/list.py
@@ -4,6 +4,42 @@ j = ["j,Smith",50]
 database = [edward,j]
 database
 [['Edward Gumby',42],["j,Smith",50]]
+greeting = "Hello"
+greeting[0]
+'H'
+greeting[-1]
+'o'
+'Hello'[1]
+'e'
+fourth =raw_input("Year: ")
+Year: 2005
+four
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
