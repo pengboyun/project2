95f678ac7ceb5b0a488e0e04d833b959d1bb1c67
Administrator <2147217957@qq.com>
2019-03-09 21:12:30 +0800

    提交测试

--- Course1/class1homework.py
+++ Course1/class1homework.py
@@ -1,1 +1,42 @@
-#coding UTF-8
+
+def sum(n):
+    Result = 0
+    Addition_number=1
+    while Addition_number < n+1:
+        Result = Result + Addition_number
+        Addition_number+=1
+    return Result
+
+
+param = 100
+print ('1加到'+str(param)+'的结果是'+ str(sum(param)))
+
+param = 1000
+print ('1加到'+str(param)+'的结果是'+ str(sum(param)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
