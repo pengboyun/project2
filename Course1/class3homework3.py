756909c176817785e36643ce17af944cbe533db1
Administrator <2147217957@qq.com>
2019-03-24 17:12:13 +0800

    提交测试

--- Course1/class3homework3.py
+++ Course1/class3homework3.py
@@ -5,7 +5,7 @@ ran = random.randint(1, 8)
 
 
 uesr_input = easygui.enterbox(str(ran)+'+3=?','皮皮看你会不会？' )
-if int(uesr_input) == ran + 3:
+if uesr_input == str(ran + 3):
     easygui.msgbox('答对了')
 else:
     easygui.msgbox('皮皮头不会！')
