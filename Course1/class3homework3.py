43a90390af0dcf6ab3e750d82b889170854e4e8c
Administrator <2147217957@qq.com>
2019-03-24 17:09:38 +0800

    提交测试

--- Course1/class3homework3.py
+++ Course1/class3homework3.py
@@ -1,8 +1,11 @@
 import easygui
 import random
-ran = random.randint(1,8)
-uesr_input = easygui.buttonbox(str(ran)+'+3=?','皮皮看你会不会？',(str(ran+3),str(ran+2),str(ran+4)))
-if str(uesr_input) == ran + 3:
+ran = random.randint(1, 8)
+
+
+
+uesr_input = easygui.enterbox(str(ran)+'+3=?','皮皮看你会不会？' )
+if int(uesr_input) == ran + 3:
     easygui.msgbox('答对了')
 else:
     easygui.msgbox('皮皮头不会！')
