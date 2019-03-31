ce56fbc42fcab2b5c3131f39a9dd8de41a726527
zjj_mac <dl_xj@126.com>
2019-03-31 20:27:20 +0800

    mac test

---
+++ Course1/class4homework5.py
@@ -0,0 +1,19 @@
+import easygui
+import random
+
+yes = random.randint(1,10)
+easygui.msgbox('猜数游戏，规则：猜1到10的数','猜数游戏','继续')
+huida = int(easygui.enterbox('你猜','请输入'))
+while True:
+    if int(huida) == yes:
+        easygui.msgbox(easygui.msgbox('答对了','猜数游戏','继续'))
+        yes = random.randint(1, 10)
+        huida = int(easygui.enterbox('你猜', '请输入'))
+
+    elif int(huida) > yes:
+        easygui.msgbox('大了')
+        huida = int(easygui.enterbox('你猜','请输入'))
+
+    elif int(huida) < yes:
+        easygui.msgbox('小了')
+        huida = int(easygui.enterbox('你猜', '请输入'))
